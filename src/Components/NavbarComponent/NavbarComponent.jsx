a8b4ef5dd34b4e86bac12a513c0c82dabfd75ece
moatazgamal100 <moatazfahmy100100@gmail.com>
2025-02-09 01:47:00 +0200

    edit

--- src/Components/NavbarComponent/NavbarComponent.jsx
+++ src/Components/NavbarComponent/NavbarComponent.jsx
@@ -1,11 +1,46 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 const NavbarComponent = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+  const words = ["needs", "lorem", "goals"];
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > window.innerHeight) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+    const wordInterval = setInterval(() => {
+      setIndex((prevIndex) => (prevIndex + 1) % words.length);
+    }, 3000);
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+      clearInterval(wordInterval);
+    };
+  });
+
   return (
     <>
       <div className="top-section">
-        <nav className="navbar navbar-expand-lg navbar-dark px-5">
+        {/* Video Background */}
+        <iframe
+          className="video-background"
+          src="https://www.youtube.com/embed/Gwnj9NgFdPE?autoplay=1&loop=1&mute=1&playlist=Gwnj9NgFdPE&controls=0&showinfo=0&modestbranding=1"
+          allow="autoplay; encrypted-media"
+        ></iframe>
+
+        {/* Navbar */}
+        <nav
+          className={`navbar navbar-expand-lg navbar-dark px-5 ${
+            isScrolled ? "fixed-navbar bg-Cerulean" : ""
+          }`}
+        >
           <div className="container-fluid">
             <Link className="navbar-brand w-50" to="#">
               <img src="/assets/logo.png" className="logo" alt="Logo" />
@@ -24,32 +59,40 @@ const NavbarComponent = () => {
             >
               <div className="navbar-nav d-flex justify-content-center align-items-center">
                 <Link
-                  className="link-color text-decoration-none py-2 px-3"
+                  className={` ${
+                    isScrolled ? "text-white" : "link-color"
+                  } text-decoration-none py-2 px-3`}
                   to="#"
                 >
                   About us
                 </Link>
                 <Link
-                  className="link-color text-decoration-none py-2 px-3 "
+                  className={` ${
+                    isScrolled ? "text-white" : "link-color"
+                  } text-decoration-none py-2 px-3`}
                   to="#"
                 >
                   Services
                 </Link>
                 <Link
-                  className="link-color text-decoration-none py-2 px-3 "
+                  className={` ${
+                    isScrolled ? "text-white" : "link-color"
+                  } text-decoration-none py-2 px-3`}
                   to="#"
                 >
                   Careers
                 </Link>
                 <Link
-                  className="link-color text-decoration-none py-2 px-3 "
+                  className={` ${
+                    isScrolled ? "text-white" : "link-color"
+                  } text-decoration-none py-2 px-3`}
                   to="#"
                 >
                   Blog
                 </Link>
                 <div className="bg-white button-border px-4 p-2">
                   <Link
-                    className="link-color text-decoration-none py-2 px-3 "
+                    className="link-color text-decoration-none py-2 px-3"
                     to="#"
                   >
                     Contact us
@@ -59,23 +102,29 @@ const NavbarComponent = () => {
             </div>
           </div>
         </nav>
-        <div className="text-overlay ">
+
+        {/* Text Overlay */}
+        <div className="text-overlay">
           <h1 className="text-white fw-bold pb-0 mb-0">
-            Full Comprehensive HR Solutions{" "}
+            Full Comprehensive HR Solutions
           </h1>
           <h2 className="text-Turquoise fw-bold pt-0 mt-0 mb-4">
-            Tailored to your business needs
+            Tailored to your{" "}
+            <span className="word-container">
+              <span className="word">{words[index]}</span>
+            </span>
           </h2>
           <div className="d-flex justify-content-center align-items-center mt-4">
             <button
               type="submit"
-              class="border-0 py-2 px-4 me-3 button-border bg-Curious-Blue text-white"
+              className="border-0 py-2 px-4 me-3 button-border bg-Curious-Blue text-white button-hover"
             >
               Contact us
             </button>
+
             <button
               type="submit"
-              class="border-Turquoise py-2 px-4 me-3 link-color button-border bg-white"
+              className="border-Turquoise py-2 px-4 me-3 link-color button-border  bg-white button-hover"
             >
               Book A meeting
             </button>
--- src/Components/RegionalAllocation/RegionalAllocation.jsx
+++ src/Components/RegionalAllocation/RegionalAllocation.jsx
@@ -1,4 +1,41 @@
-import React from "react";
+import React, { useEffect, useState, useRef } from "react";
+
+const Counter = ({ targetNumber, suffix = "" }) => {
+  const [count, setCount] = useState(0);
+  const ref = useRef(null);
+
+  useEffect(() => {    
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          let start = 0;
+          const duration = 2000; // Animation duration in milliseconds
+          const stepTime = Math.abs(Math.floor(duration / targetNumber));
+          const timer = setInterval(() => {
+            start += 1;
+            if (start > targetNumber) {
+              clearInterval(timer);
+              setCount(targetNumber);
+            } else {
+              setCount(start);
+            }
+          }, stepTime);
+        }
+      },
+      { threshold: 0.5 }
+    );
+
+    if (ref.current) observer.observe(ref.current);
+    return () => observer.disconnect();
+  }, [targetNumber]);
+
+  return (
+    <h2 ref={ref} className="fw-bolder text-Cerulean fs-50px">
+      {count}
+      {suffix}
+    </h2>
+  );
+};
 
 const RegionalAllocation = () => {
   return (
@@ -12,11 +49,20 @@ const RegionalAllocation = () => {
           <div className="container-fluid">
             <div className="row position-relative " style={{ zIndex: 1 }}>
               <div className="col-md-6 d-flex align-items-center justify-content-center py-5">
-                <img
-                  src="/assets/4300496-hd_720_962_30fps 1.png"
-                  alt=""
-                  className="w-60 position-relative"
-                />
+                {/* <video
+                  src="/assets/diamond.mp4"
+                  className="diamond "
+                  autoPlay
+                  loop
+                  muted
+                /> */}
+                <iframe
+                  className="rounded-5"
+                   width="80%" 
+                   height="400px" 
+                  src="https://www.youtube.com/embed/2Jxl5nKeuPc?autoplay=1&loop=1&mute=1&playlist=2Jxl5nKeuPc&controls=0&modestbranding=1"
+                  allow="autoplay; encrypted-media"
+                ></iframe>
               </div>
               <div className="col-md-6 d-flex flex-column align-items-center align-items-md-start justify-content-center pt-4">
                 <div className="w-50">
@@ -48,18 +94,14 @@ const RegionalAllocation = () => {
                   <div className="row">
                     <div className="col-lg-6">
                       <div className="bg-Alice-Blue mb-3 d-flex flex-column h-30vh align-items-start justify-content-center rounded-4 p-4">
-                        <h2 className="fw-bolder text-Cerulean fs-50px ">
-                          +100
-                        </h2>
+                        <Counter targetNumber={100} suffix="+" />
                         <h6 className="fw-light text-Dark-Gray">
                           Companies we have partnered with worldwide.
                         </h6>
                       </div>
                       <div className="bg-Alice-Blue mb-3 d-flex flex-column h-30vh align-items-start justify-content-center rounded-4 p-4">
-                        <p className=" text-Cerulean m-0 p-0">Fortune</p>
-                        <h2 className="fw-bolder text-Cerulean fs-50px ">
-                          500
-                        </h2>
+                        <p className="text-Cerulean m-0 p-0">Fortune</p>
+                        <Counter targetNumber={500} />
                         <h6 className="fw-light text-Dark-Gray">
                           We have partnered with clients prominently featured on
                           the Fortune 500.
@@ -67,18 +109,14 @@ const RegionalAllocation = () => {
                       </div>
                     </div>
                     <div className="col-lg-6">
-                      <div className="bg-Alice-Blue mb-3 d-flex flex-column h-30vh align-items-start justify-content-center  rounded-4 p-4">
-                        <h2 className="fw-bolder text-Cerulean fs-50px ">
-                          20+
-                        </h2>
+                      <div className="bg-Alice-Blue mb-3 d-flex flex-column h-30vh align-items-start justify-content-center rounded-4 p-4">
+                        <Counter targetNumber={20} suffix="+" />
                         <h6 className="fw-light text-Dark-Gray">
                           Different departments & industries we’ve worked in.
                         </h6>
                       </div>
-                      <div className="bg-Alice-Blue mb-3 d-flex flex-column h-30vh align-items-start justify-content-center  rounded-4 p-4">
-                        <h2 className="fw-bolder text-Cerulean fs-50px">
-                          100+
-                        </h2>
+                      <div className="bg-Alice-Blue mb-3 d-flex flex-column h-30vh align-items-start justify-content-center rounded-4 p-4">
+                        <Counter targetNumber={100} suffix="+" />
                         <h6 className="fw-light text-Dark-Gray">
                           Outsourcing deals with companies from all over the
                           EMEA & The Americas Regions.
